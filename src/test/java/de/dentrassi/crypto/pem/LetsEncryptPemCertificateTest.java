deae7ec9dc462fd2af3ce1e3a3f7ba6171b58e03
Sergio Moreno <chechunawer@gmail.com>
2018-12-17 22:03:16 +0100

    Let's encrypt fullchain/key pem file

--- src/test/java/de/dentrassi/crypto/pem/LetsEncryptPemCertificateTest.java
+++ src/test/java/de/dentrassi/crypto/pem/LetsEncryptPemCertificateTest.java
@@ -3,6 +3,7 @@ package de.dentrassi.crypto.pem;
 import java.io.FileInputStream;
 import java.security.Key;
 import java.security.KeyStore;
+import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 
 import org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPrivateCrtKey;
@@ -21,11 +22,11 @@ public class LetsEncryptPemCertificateTest {
 		java.security.cert.Certificate cert = ks.getCertificate("letsencrypt");
 		X509Certificate x509 = (X509Certificate) cert;
 		Key key = ks.getKey("letsencrypt", new char[0]);
-		String actualName = x509.getSubjectDN().getName();
-		String expectedName = "CN=sergiomoreno.eu";
 		Assert.assertEquals(x509.getSubjectAlternativeNames().size(), 2);
-		Assert.assertEquals(expectedName, actualName);
 		Assert.assertNotNull(key);
 		Assert.assertEquals(key.getClass(), BCRSAPrivateCrtKey.class);
+		Certificate[] chain = ks.getCertificateChain("letsencrypt");
+		Assert.assertNotNull(chain);
+		Assert.assertEquals(2, chain.length);
 	}
 }
