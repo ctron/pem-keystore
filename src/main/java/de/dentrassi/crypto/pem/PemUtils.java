f1c1f4b54ca594500f3065aaafa73e3a427750a9
Sergio Moreno <chechunawer@gmail.com>
2019-01-12 12:06:13 +0100

    White space issue.
    Signed-off-by: Sergio Moreno <chechunawer@gmail.com>

--- src/main/java/de/dentrassi/crypto/pem/PemUtils.java
+++ src/main/java/de/dentrassi/crypto/pem/PemUtils.java
@@ -42,88 +42,87 @@ import de.dentrassi.crypto.pem.AbstractPemKeyStore.Entry;
 
 public class PemUtils {
 
-	private static final String SOURCE_PREFIX = "source.";
+    private static final String SOURCE_PREFIX = "source.";
 
 	public static Map<String, Entry> loadFrom(final InputStream stream, final boolean chained) throws CertificateException, IOException {
 
-		final Map<String, Entry> result = new HashMap<>();
+        final Map<String, Entry> result = new HashMap<>();
 
-		loadFrom(result, "pem", chained, stream);
+        loadFrom(result, "pem", chained, stream);
 
-		return Collections.unmodifiableMap(result);
+        return Collections.unmodifiableMap(result);
 	}
 
 	public static Map<String, Entry> loadFromConfiguration(final InputStream stream) throws CertificateException, IOException {
 
-		final Map<String, Entry> result = new HashMap<>();
+        final Map<String, Entry> result = new HashMap<>();
 
-		final Properties p = new Properties();
-		p.load(stream);
+        final Properties p = new Properties();
+        p.load(stream);
 
-		final String alias = p.getProperty("alias", "pem");
+        final String alias = p.getProperty("alias", "pem");
 
-		for (final String key : p.stringPropertyNames()) {
-			if (key.startsWith(SOURCE_PREFIX)) {
-				try (FileInputStream source = new FileInputStream(new File(p.getProperty(key)))) {
-					loadFrom(result, alias, true, source);
-				}
-			}
-		}
+        for (final String key : p.stringPropertyNames()) {
+            if (key.startsWith(SOURCE_PREFIX)) {
+                try (FileInputStream source = new FileInputStream(new File(p.getProperty(key)))) {
+                    loadFrom(result, alias, true, source);
+                }
+            }
+        }
 
-		return result;
+        return result;
 
-	}
+    }
 
-	private static void loadFrom(final Map<String, Entry> result, final String alias, final boolean chained, final InputStream stream) throws CertificateException, IOException {
+    private static void loadFrom(final Map<String, Entry> result, final String alias, final boolean chained, final InputStream stream) throws CertificateException, IOException {
 
-		final CertificateFactory factory = CertificateFactory.getInstance("X.509");
-		final JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider(new BouncyCastleProvider());
+        final CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        final JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider(new BouncyCastleProvider());
 
-		@SuppressWarnings("resource")
-		final PEMParser reader = new PEMParser(new InputStreamReader(stream, StandardCharsets.UTF_8));
+        @SuppressWarnings("resource")
+        final PEMParser reader = new PEMParser(new InputStreamReader(stream, StandardCharsets.UTF_8));
 
-		final List<Certificate> chain = new ArrayList<>();
-		Key key = null;
-		int counter = 0;
+        final List<Certificate> chain = new ArrayList<>();
+        Key key = null;
+        int counter = 0;
 
-		Object object;
-		while ((object = reader.readObject()) != null) {
+        Object object;
+        while ((object = reader.readObject()) != null) {
 
-			if (object instanceof X509CertificateHolder) {
+            if (object instanceof X509CertificateHolder) {
 
-				final X509CertificateHolder certHolder = (X509CertificateHolder) object;
+                final X509CertificateHolder certHolder = (X509CertificateHolder) object;
 
-				final Collection<? extends Certificate> certs = factory.generateCertificates(new ByteArrayInputStream(certHolder.getEncoded()));
+                final Collection<? extends Certificate> certs = factory.generateCertificates(new ByteArrayInputStream(certHolder.getEncoded()));
 
-				for (final Certificate cert : certs) {
-					if (chained) {
-						if (cert instanceof X509Certificate) {
-							chain.add(cert);
-						}
-					} else {
-						result.put(alias + "-" + counter++, new Entry(null, new Certificate[] { cert }));
-					}
-				}
+                for (final Certificate cert : certs) {
+                    if (chained) {
+                        if (cert instanceof X509Certificate) {
+                            chain.add(cert);
+                        }
+                    } else {
+                    	result.put(alias + "-" + counter++, new Entry(null, new Certificate[] { cert }));
+                    }
+                }
+            } else if (object instanceof PEMKeyPair) {
+                key = converter.getKeyPair((PEMKeyPair) object).getPrivate();
+            } else if (object instanceof PrivateKeyInfo) {
+                key = converter.getPrivateKey((PrivateKeyInfo) object);
+            }
+        }
 
-			} else if (object instanceof PEMKeyPair) {
-				key = converter.getKeyPair((PEMKeyPair) object).getPrivate();
-			} else if (object instanceof PrivateKeyInfo) {
-				key = converter.getPrivateKey((PrivateKeyInfo) object);
-			}
-		}
+        final Certificate[] certificateChain = chain.isEmpty() ? null : chain.toArray(new X509Certificate[chain.size()]);
 
-		final Certificate[] certificateChain = chain.isEmpty() ? null : chain.toArray(new X509Certificate[chain.size()]);
+        final Entry e = new Entry(key, certificateChain);
 
-		final Entry e = new Entry(key, certificateChain);
+        result.compute(alias, (k, v) -> {
+            if (v != null) {
+                return v.merge(e);
+            } else {
+                return e;
+            }
+        });
 
-		result.compute(alias, (k, v) -> {
-			if (v != null) {
-				return v.merge(e);
-			} else {
-				return e;
-			}
-		});
-
-	}
+    }
 
 }
