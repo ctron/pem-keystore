26e9fe1185aeccd81c2fc8f90cd5f82ce1cc8ca9
Jens Reimann <jreimann@redhat.com>
2020-04-17 15:27:30 +0200

    Add an additional test for files without "file:" prefix

--- src/test/java/de/dentrassi/crypto/pem/PemUtilsTest.java
+++ src/test/java/de/dentrassi/crypto/pem/PemUtilsTest.java
@@ -11,19 +11,27 @@
 
 package de.dentrassi.crypto.pem;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class PemUtilsTest {
 
     @Test
     void loadFromConfigurationClasspath() throws CertificateException, IOException {
         Map<String, AbstractPemKeyStore.Entry> map = PemUtils.loadFromConfiguration(getClass().getResourceAsStream("/classpath_tls.properties"));
+        AbstractPemKeyStore.Entry entry = map.get("keycert");
+        assertThat(entry.getKey()).isNotNull();
+        assertThat(entry.getCertificate()).isNotNull();
+    }
+
+    @Test
+    void loadFromConfigurationFileUrl() throws CertificateException, IOException {
+        Map<String, AbstractPemKeyStore.Entry> map = PemUtils.loadFromConfiguration(getClass().getResourceAsStream("/file_url_tls.properties"));
         AbstractPemKeyStore.Entry entry = map.get("keycert");
         assertThat(entry.getKey()).isNotNull();
         assertThat(entry.getCertificate()).isNotNull();
